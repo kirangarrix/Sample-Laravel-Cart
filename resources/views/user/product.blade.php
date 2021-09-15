bcc3e03f1aa8170ea034ae1ba27927d5fed45fa0
kiran garrix <kirangarrix2019@gmail.com>
2021-09-15 22:57:58 +0530

    sample ecommercet

--- resources/views/user/product.blade.php
+++ resources/views/user/product.blade.php
@@ -20,7 +20,7 @@
 
         <div class="col-md-4">
           <div class="product-item">
-            <a href="#"><img height="300" width="150" src="/productimage/{{ $product->image }}" alt=""></a>
+            <a href="#"><img height="300" width="180" src="/productimage/{{ $product->image }}" alt=""></a>
             <div class="down-content">
               <a href="#"><h4>{{ $product->title }}</h4></a>
               <h6>${{ $product->price }}</h6>
