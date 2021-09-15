b9cfeb65c7c3ab08f0aaa77cc3ea781c9c83458f
kiran garrix <kirangarrix2019@gmail.com>
2021-09-15 21:20:43 +0530

    first commit

---
+++ resources/views/admin/product.blade.php
@@ -0,0 +1,87 @@
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+ @include('admin.css')
+
+ <style type="text/css">
+   
+   .title{
+    color: white;
+     padding-top: 25px;
+      font-size: 25px;
+   }
+
+   label{
+     display: inline-block;
+     width: 200px;
+   }
+
+ </style>
+  </head>
+  <body>
+
+      <!-- partial -->
+      @include('admin.sidebar')
+
+      @include('admin.navbar')
+      
+      <!-- partial -->
+      <div class="container-fluid page-body-wrapper">
+          <div class="container" align="center">
+        
+            <h1 class="title">Add Product</h1>
+       
+       @if(session()->has('message'))
+         <div class="alert alert-success">
+       <button type="button" class="close" data-dismiss="alert">X</button>
+              {{ session()->get('message') }}
+         
+            </div>     
+       @endif
+       
+            <form action="{{ url('uploadproduct') }}" method="POST" enctype="multipart/form-data">
+
+          @csrf
+        <div style="padding: 15px">
+          <label for="Product title">Product Title</label>
+          <input style="color: black" type="text" name="title" placeholder="Give a product title" required>
+        </div>
+
+        <div style="padding: 15px">
+          <label for="Product title">Price</label>
+          <input style="color: black" type="number" name="price" placeholder="Give a price" required>
+        </div>
+
+        <div style="padding: 15px">
+          <label for="Product title">Description</label>
+          <input style="color: black" type="text" name="des" placeholder="Give a product Description" required>
+        </div>
+
+        <div style="padding: 15px">
+          <label for="Product title">Quantity</label>
+          <input style="color: black" type="text" name="quantity" placeholder="Give a product Quatity" required>
+        </div>
+
+        <div style="padding: 15px">
+          <input type="file" name="file">
+        </div>
+
+        <div style="padding: 15px">
+        <input class="btn btn-success" type="submit" name="">
+        </div>
+
+      </form>
+
+
+
+
+           </div>  
+       </div>
+
+    
+       
+          <!-- partial -->
+      @include('admin.script')
+  </body>
+</html>
